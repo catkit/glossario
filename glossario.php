2ad0de757a10cdcf8a2b4e608ffdc2bd1cadb62d
Vinicius Massuchetto <vmassuchetto@gmail.com>
2013-12-26 14:18:12 -0200

    Render terms table using jquery-data-tables

--- glossario.php
+++ glossario.php
@@ -22,6 +22,9 @@ class Glossario {
 	function Glossario() {
 		add_action( 'init', array( $this, 'init' ) );
 		add_action( 'admin_init', array( $this, 'admin_init' ) );
+		add_action( 'the_posts', array( $this, 'the_posts' ) );
+		add_action( 'wp_ajax_glossario', array( $this, 'wp_ajax' ) );
+		add_action( 'wp_ajax_nopriv_glossario', array( $this, 'wp_ajax' ) );
 	}
 
 	function activate() {
@@ -199,6 +202,154 @@ class Glossario {
 			register_taxonomy( $taxonomy, $args['object_types'], $args );
 		}
 	}
+
+	function get_terms( $args = false ) {
+
+		global $wpdb;
+
+		$defaults = array(
+			'iDisplayLength' => 50,
+			'iDisplayStart' => 0,
+			'iSearch' => false,
+			'orderby' => 'post_title'
+
+		);
+		$args = wp_parse_args( $args, $defaults );
+
+		$select_from = "
+			SELECT
+				p.ID          AS 'term_id',
+				os.meta_value AS 'original_term_singular',
+				op.meta_value AS 'original_term_plural',
+				ts.meta_value AS 'term_singular',
+				tp.meta_value AS 'term_plural'
+			FROM
+				{$wpdb->posts} p ";
+
+		$join = "
+			LEFT JOIN {$wpdb->postmeta} os ON 1=1
+				AND p.ID = os.post_id
+				AND os.meta_key = '" . Glossario::$slug . "_original_term_singular'
+			LEFT JOIN {$wpdb->postmeta} op ON 1=1
+				AND p.ID = op.post_id
+				AND op.meta_key = '" . Glossario::$slug . "_original_term_plural'
+			LEFT JOIN {$wpdb->postmeta} ts ON 1=1
+				AND p.ID = ts.post_id
+				AND ts.meta_key = '" . Glossario::$slug . "_term_singular'
+			LEFT JOIN {$wpdb->postmeta} tp ON 1=1
+				AND p.ID = tp.post_id
+				AND tp.meta_key = '" . Glossario::$slug . "_term_plural' ";
+
+		$where = "
+			WHERE 1=1
+				AND post_type = '" . Glossario::$post_term . "'
+				AND post_status = 'publish' ";
+
+		if ( !empty( $args['iSearch'] ) )
+			$where .= $wpdb->prepare( "AND (
+				original_term_singular LIKE '%%%s%'
+				OR original_term_plural LIKE '%%%s%'
+				OR term_singular LIKE '%%%s%'
+				OR term_plural LIKE '%%%s%' ) ",
+				$args['iSearch'], $args['iSearch'],
+				$args['iSearch'], $args['iSearch'] );
+
+		$orderby_limit = $wpdb->prepare( "
+			ORDER BY %s
+			LIMIT %d, %d ",
+			$args['orderby'],
+			$args['iDisplayStart'],
+			$args['iDisplayLength'] );
+
+		$sql = $select_from . $join . $where . $orderby_limit;
+		$query = $wpdb->get_results( $sql );
+		return $query;
+	}
+
+	function shortcode_term_table() {
+		$id = Glossario::$slug . '-' . uniqid() . '-term-table';
+		ob_start();
+		?>
+		<table id="<?php echo $id; ?>" class="<?php echo  Glossario::$slug . '-term-table'; ?>">
+
+			<thead>
+			<tr clss="term-titles">
+				<th class="term-original-singular"><?php _e( 'Original singular', 'glossario' ); ?></th>
+				<th class="term-original-plural"><?php _e( 'Original plural', 'glossario' ); ?></th>
+				<th class="term-singular"><?php _e( 'Translated singular', 'glossario' ); ?></th>
+				<th class="term-plural"><?php _e( 'Translated plural', 'glossario' ); ?></th>
+			</tr>
+			</thead>
+
+		</table>
+		<script type="text/javascript">
+			jQuery(document).ready(function(){
+				jQuery('#<?php echo $id; ?>').dataTable({
+				    'bProcessing': true,
+					'bServerSide': true,
+					'sPaginationType': 'full_numbers',
+					'sAjaxSource': '<?php echo admin_url('admin-ajax.php') . '?action=glossario&call=term_list'; ?>'
+				});
+			});
+		</script>
+		<?php
+		return ob_get_clean();
+	}
+
+	function wp_ajax() {
+
+		if ( empty( $_GET['call'] ) )
+			return false;
+
+		if ( 'term_list' == $_GET['call'] ) {
+
+			$response = array(
+				'iTotalRecords' => wp_count_posts( Glossario::$post_term )->publish,
+				'iTotalDisplayRecords' => wp_count_posts( Glossario::$post_term )->publish,
+			);
+
+			foreach ( $this->get_terms( $_GET ) as $term ) {
+				$permalink = get_permalink( $term->term_id );
+				$link = '<a href="' . $permalink . '">%s</a>';
+				$terms[] = array(
+					sprintf( $link, $term->original_term_singular ),
+					sprintf( $link, $term->original_term_plural ),
+					sprintf( $link, $term->term_singular ),
+					sprintf( $link, $term->term_plural )
+				);
+			}
+
+			$response['aaData'] = $terms;
+			echo json_encode( $response );
+			exit();
+
+		}
+
+	}
+
+	/**
+	 * Check if some of the plugin's shortcodes are being used just after the
+	 * posts are rewtrieved from the database, and if so, enqueue the scripts
+	 * needed.
+	 */
+	function the_posts( $posts ) {
+
+		if ( empty( $posts ) )
+			return $posts;
+
+		$found = false;
+		foreach ( $posts as $post ) {
+			if ( preg_match( '#\[' . Glossario::$slug . '_#', $post->post_content ) )
+				$found = true;
+				break;
+		}
+
+		if ( $found )
+			wp_enqueue_script( 'jquery-data-tables',  plugins_url() . '/'. Glossario::$slug . '/js/jquery-data-tables.min.js', array( 'jquery' ) );
+
+		return $posts;
+	}
+
 }
 
 function glossario_plugins_loaded() {
@@ -209,3 +360,5 @@ add_action( 'plugins_loaded', 'glossario_plugins_loaded' );
 register_activation_hook( __FILE__, array( 'Glossario', 'activate' ) );
 register_deactivation_hook( __FILE__, array( 'Glossario', 'deactivate' ) );
 register_uninstall_hook( __FILE__, array( 'Glossario', 'uninstall' ) );
+
+add_shortcode( 'glossario_term_table', array( 'Glossario', 'shortcode_term_table' ) );
